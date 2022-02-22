bdd567bfe0535eb1c1babe987cc7583812637e49
Marcos Della Justina Schulz <markosdjs@gmail.com>
2022-02-22 10:26:08 -0300

    Adicionado o verbos

--- 03-Person/src/main/java/com/schulzmarcos/api/resources/PersonController.java
+++ 03-Person/src/main/java/com/schulzmarcos/api/resources/PersonController.java
@@ -1,9 +1,9 @@
 package com.schulzmarcos.api.resources;
 
 import com.schulzmarcos.domain.models.Person;
-import com.schulzmarcos.domain.services.ConverterService;
 import com.schulzmarcos.domain.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,5 +24,25 @@ public class PersonController {
     @GetMapping("/{id}")
     public Person findById(@PathVariable("id") String id){
         return service.findById(id);
+    }
+
+    @RequestMapping(method=RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @RequestMapping(method=RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return service.update(person);
+    }
+
+    @RequestMapping(value="/{id}",
+            method=RequestMethod.DELETE)
+    public void delete(@PathVariable("id") String id) {
+        service.delete(id);
     }
 }
