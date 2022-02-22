00449e40bf0b5cf5c09a605a6d1d6b0b5a1190f7
Marcos Della Justina Schulz <markosdjs@gmail.com>
2022-02-22 17:34:03 -0300

    adicionado anotação a configuração

--- 04-greeting-service/src/main/java/com/marcosschulz/greetingservice/configuration/GreetingConfiguration.java
+++ 04-greeting-service/src/main/java/com/marcosschulz/greetingservice/configuration/GreetingConfiguration.java
@@ -1,9 +1,11 @@
 package com.marcosschulz.greetingservice.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope //anotação para fazer atualização das configurações dos microserviços para não precisar reiniciar sempre que haver alteração, atraves do refresh gerado pelo ACTUATOR é so roda-lo como post e ele obriga a atualização das alterações feitas
 @ConfigurationProperties("greeting-service")
 public class GreetingConfiguration {
 
