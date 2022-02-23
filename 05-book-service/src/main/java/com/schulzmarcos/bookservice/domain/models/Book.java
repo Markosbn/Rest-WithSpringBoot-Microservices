b73554cc355f52a0f420d1142eb349d9b8340ed0
Marcos Della Justina Schulz <markosdjs@gmail.com>
2022-02-23 14:19:51 -0300

    Ajustado erro Serialização jackson

--- 05-book-service/src/main/java/com/schulzmarcos/bookservice/domain/models/Book.java
+++ 05-book-service/src/main/java/com/schulzmarcos/bookservice/domain/models/Book.java
@@ -1,11 +1,14 @@
 package com.schulzmarcos.bookservice.domain.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
 @Entity(name = "book")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Book implements Serializable {
     private static final long serialVersionUID = 1L;
 
