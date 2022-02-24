8f89206cf95bd1f2bbd02403aa4a42915746b1cf
Marcos Della Justina Schulz <markosdjs@gmail.com>
2022-02-24 16:28:32 -0300

    Adicionado RateLimiter e Bulkhead

--- 06-book-service/src/main/java/com/schulzmarcos/bookservice/api/resources/TestController.java
+++ 06-book-service/src/main/java/com/schulzmarcos/bookservice/api/resources/TestController.java
@@ -1,6 +1,8 @@
 package com.schulzmarcos.bookservice.api.resources;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,9 +18,11 @@ public class TestController {
     //implementado o logger para fazermos o track das requisições
     private Logger logger = LoggerFactory.getLogger(TestController.class);
 
-    @GetMapping("/test")                                    //name, é o nome dado para esta endpoint. fall
+    @GetMapping("/test")                                    //name, é o nome dado para esta endpoint. fallbackMethod retorna uma excessão apos falhar determinadas vezes
     //@Retry(name= "default", fallbackMethod = "metodoRetorno")//implementado o retry, para quando a requisição não for concluida ele tentar novamente, por padrão o retry testa 3 vezes, mas pode ser configurado a quantidade pelos propriedades do app
-    @CircuitBreaker(name = "default", fallbackMethod = "metodoRetorno") //Funciona como um disjuntor, onde se ha falhas ignora a requisição principal e vai direto para o fallbackMethod, e dps testa aos poucos, caso estabilize as falhas, retorna ao metodo principal, se não faz novamente o pulo para o fallback
+    //@CircuitBreaker(name = "default", fallbackMethod = "metodoRetorno") //Funciona como um disjuntor, onde se ha falhas ignora a requisição principal e vai direto para o fallbackMethod, e dps testa aos poucos, caso estabilize as falhas, retorna ao metodo principal, se não faz novamente o pulo para o fallback
+    //@RateLimiter(name = "default")//limita o tanto de requisições por um periodo x de tempo, assim como o retry, pode ser configurado por perfil
+    @Bulkhead(name = "default")//limita o quantidade de requisições concorrentes.
     public String test(){
         logger.info("request para test recebido!!");//implementado o logger para fazermos o track das requisições
 
