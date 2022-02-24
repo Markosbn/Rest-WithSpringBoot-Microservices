c293365b606d2cb5679895b1205607c117a2936f
Marcos Della Justina Schulz <markosdjs@gmail.com>
2022-02-24 16:12:35 -0300

    adicionado o circuitBreaker

--- 06-book-service/src/main/java/com/schulzmarcos/bookservice/api/resources/TestController.java
+++ 06-book-service/src/main/java/com/schulzmarcos/bookservice/api/resources/TestController.java
@@ -1,5 +1,6 @@
 package com.schulzmarcos.bookservice.api.resources;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +17,8 @@ public class TestController {
     private Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/test")                                    //name, é o nome dado para esta endpoint. fall
-    @Retry(name= "default", fallbackMethod = "metodoRetorno")//implementado o retry, para quando a requisição não for concluida ele tentar novamente, por padrão o retry testa 3 vezes, mas pode ser configurado a quantidade pelos propriedades do app
+    //@Retry(name= "default", fallbackMethod = "metodoRetorno")//implementado o retry, para quando a requisição não for concluida ele tentar novamente, por padrão o retry testa 3 vezes, mas pode ser configurado a quantidade pelos propriedades do app
+    @CircuitBreaker(name = "default", fallbackMethod = "metodoRetorno") //Funciona como um disjuntor, onde se ha falhas ignora a requisição principal e vai direto para o fallbackMethod, e dps testa aos poucos, caso estabilize as falhas, retorna ao metodo principal, se não faz novamente o pulo para o fallback
     public String test(){
         logger.info("request para test recebido!!");//implementado o logger para fazermos o track das requisições
 
