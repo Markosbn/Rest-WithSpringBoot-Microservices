0f88aa5dc835183437be40de4fe0e40a54d3d099
Marcos Della Justina Schulz <markosdjs@gmail.com>
2022-02-24 13:39:43 -0300

    Adicionado o filter para o apiGateway, para trazer um filtro das requisições realizadas

---
+++ 06-api-gateway/src/main/java/com/schulzmarcos/apigateway/api/filter/LoggingFilter.java
@@ -0,0 +1,23 @@
+package com.schulzmarcos.apigateway.api.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+//classe criada para gerenciar as logs, com filtros de transações realizadas.
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+    //chama o logger
+    private Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override //chama a mensagem, e a requisição. passando o caminho acessado.
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+      //  logger.info("Original request path -> {}", exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+}
